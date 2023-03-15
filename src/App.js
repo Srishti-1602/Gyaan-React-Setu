7baec6ba38c1121740f93265d590131dc5268542
Srishti Agrawal <88929699+Srishti-1602@users.noreply.github.com>
2023-03-15 07:49:28 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,31 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/style-prop-object */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+// import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './App.css';
+import React from 'react';
+// import Navbar from './Nav/Nav';
+// import { YourTopics, SaveNotesDiv } from './Main/Main';
+// import Main from './Main/Main';
+import Start from './FirstPage/First';
+
+// import logo from './Images/Gyaan setu.png';
+
+function App() {
+return (
+    <div>
+      <Start />
+      {/* <Navbar />
+      <YourTopics />
+      <Main />
+      <SaveNotesDiv/> */}
+    </div>
+   
+  );
+}
+
+
+export default App;
