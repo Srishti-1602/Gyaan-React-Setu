9eb099f2e7a5e86e2b6fd049117d411b8507f337
Srishti Agrawal <88929699+Srishti-1602@users.noreply.github.com>
2023-03-16 22:43:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 import React from 'react';
-// import Navbar from './Nav/Nav';
+import Navbar from './Nav/Nav';
 // import { YourTopics, SaveNotesDiv } from './Main/Main';
 // import Main from './Main/Main';
 import Start from './FirstPage/First';
+import Pricing from './Community/Community';
 
 // import logo from './Images/Gyaan setu.png';
 
 function App() {
 return (
     <div>
-      <Start />
+      <Navbar />
+      <Pricing />
+      {/* <Start /> */}
       {/* <Navbar />
       <YourTopics />
       <Main />
