cad252149c3ee6a723903ad190e3a39a382d3690
Sukritee Sharma <87569188+aceta-minophen@users.noreply.github.com>
2023-06-23 00:25:09 +0530

    signin successfully implemented

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,34 +1,81 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './login.css';
 // import logo from '../Images/Gyaan setu.png';
 import logimg from '../Images/6059971.jpg';
-// import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../firebaseConfig';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getDatabase, ref, onValue, set } from 'firebase/database';
+import firebase from 'firebase/app';
+import 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
+import * as firebaseui from 'firebaseui';
 
-
-function LogIn() {
+function Login() {
   const navigate = useNavigate();
 
   const handleSignUpClick = () => {
     navigate('/SignUp');
   };
 
-  const handleGoogleSignIn = () => {
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth(); // Assuming you have initialized the Firebase app properly
+  useEffect(() => {
+    const uiConfig = {
+      signInSuccessUrl: '/SignUp', // Redirect URL after successful sign-in
+      signInFlow: 'popup', // Display a pop-up window for sign-in
+      signInOptions: [
+        {
+          provider: GoogleAuthProvider.PROVIDER_ID,
+          customParameters: {
+            prompt: 'select_account',
+          },
+        },
+      ], // Enable Google sign-in only
+      callbacks: {
+        signInSuccessWithAuthResult: (authResult) => {
+          const user = authResult.user;
+          const userId = user.uid;
 
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // Handle successful Google sign-in here
-        console.log(result.user);
-      })
-      .catch((error) => {
-        // Handle Google sign-in error here
-        console.log(error);
+          // Check if the user exists in the 'users' node of the Realtime Database
+      const database = getDatabase();
+      const userRef = ref(database, `users/${userId}`);
+
+      onValue(userRef, (snapshot) => {
+        const userData = snapshot.val();
+
+        if (userData) {
+          // User exists in the database
+          if (userData.registered === false) {
+            navigate('/SignUp');
+          } else {
+            navigate('/index');
+          }
+        } else {
+          // User doesn't exist in the database, set the registration value to false
+          set(userRef, { registered: false })
+            .then(() => {
+              console.log('User added successfully.');
+              navigate('/SignUp');
+            })
+            .catch((error) => {
+              console.error('Error adding user:', error);
+            });
+        }
       });
-  };
+
+          //navigate('/SignUp'); // Redirect to the signup page
+          return false; // Avoid redirecting automatically
+        },
+        signInFailure: (error) => {
+          // Handle sign-in failure scenarios
+          console.error(error);
+        },
+      },
+    };
+
+    const auth = getAuth();
+    const ui = new firebaseui.auth.AuthUI(auth);
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }, [navigate]);
 
   return (
     <>
@@ -38,8 +85,12 @@ function LogIn() {
           <div className='Info'>
             <h3 className='welcome'>Welcome to</h3>
             <h3 className='gyaan'>Gyaan Setu</h3>
-            <button onClick={handleGoogleSignIn}>Sign in with Google</button>
-            <p className='Para1'>Don't have an account? <span onClick={handleSignUpClick} style={{ color: 'yellow', cursor: 'pointer' }}>Sign Up</span>
+            <div id="firebaseui-auth-container"></div>
+            <p className='Para1'>
+              Don't have an account?{' '}
+              <span onClick={handleSignUpClick} style={{ color: 'yellow', cursor: 'pointer' }}>
+                Sign Up
+              </span>
             </p>
           </div>
         </div>
@@ -48,5 +99,4 @@ function LogIn() {
   );
 }
 
-
-export default LogIn;
+export default Login;
