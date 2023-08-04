3fff59e8b89da5c85c5646fbf6afedb0c336035a
Sukritee Sharma <87569188+aceta-minophen@users.noreply.github.com>
2023-08-04 23:36:35 +0530

    add new subtopics done

--- src/Main/JSONnode/JSONcomponents/searchNewTopic.jsx
+++ src/Main/JSONnode/JSONcomponents/searchNewTopic.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Spinner } from "react-bootstrap";
+import apiComm from "../../Search/backendIntegration"
 const { v4: uuidv4 } = require('uuid');
 
 const SearchInput = ({ nodeKey, value, mydata, setData }) => {
   const [inputValue, setInputValue] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (event) => {
     setInputValue(event.target.value);
@@ -11,36 +13,48 @@ const SearchInput = ({ nodeKey, value, mydata, setData }) => {
 
   const uuid = uuidv4().replace(/-/g, ''); // Remove dashes from the UUID
 
-  const handleButtonClick = () => {
-    const updateNestedData = (data, key, value) => {
-      // Base case: If the current data is not an object, return the current data as is
-      if (typeof data !== "object" || data === null) {
-        return data;
-      }
-  
-      // Recursively update nested data
-      const updatedData = Array.isArray(data) ? [...data] : { ...data };
-      for (let prop in updatedData) {
-        if (prop === key) {
-          updatedData[prop] = value;
-          break; // Stop updating subnodes once the key is found and updated
-        } else {
-          updatedData[prop] = updateNestedData(updatedData[prop], key, value);
-        }
-      }
-  
-      return updatedData;
-    };
-  
-    // Update the mydata object with the inputted value under the nodeKey using recursion
-    const updatedData = updateNestedData(mydata, nodeKey, inputValue);
-  
-    // Update the state to re-render the component with the updated data
-    setData(updatedData);
+  let nestedData = {
+    [uuid + '_' + 'Subtitle']: ['Add Your Topic Content Here'],
+    ['Subtitle2']: ['Add Your Topic Content Here'],
   };
-  
-  
-  
+
+  const handleSubtopicSearch = async () => {
+    setLoading(true);
+    // Call the API with the inputValue as the query
+    try {
+      const apiResponse = await apiComm(inputValue);
+      const apiData = JSON.parse(apiResponse.data);
+
+      // Merge the inputValue with the first 32 characters of the nodeKey
+      const updatedNodeKey = nodeKey.slice(0, 32) + '_' + inputValue;
+
+      // Create a copy of mydata to avoid directly mutating the state
+      const newData = { ...mydata };
+
+      // Update the nested dictionary with the new nodeKey and API data
+      const updateNestedDict = (data, key, newKey, apiData) => {
+        if (data[key]) {
+          data[newKey] = apiData;
+          delete data[key];
+        }
+        Object.keys(data).forEach((k) => {
+          if (typeof data[k] === 'object') {
+            updateNestedDict(data[k], key, newKey, apiData);
+          }
+        });
+      };
+
+      updateNestedDict(newData, nodeKey, updatedNodeKey, apiData);
+
+      // Set the updated nested dictionary using setData
+      setData(newData);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching data from API:", error);
+      setLoading(false);
+    }
+  };
+    
 
   return (
     <span style={{ display: "inline-flex", alignItems: "center" }}>
@@ -57,7 +71,9 @@ const SearchInput = ({ nodeKey, value, mydata, setData }) => {
           minWidth: "100px", // Optional: Set a minimum width to avoid shrinking too much
         }}
       />
-      <Button onClick={() => handleButtonClick()}>{value}</Button>
+      <Button onClick={() => handleSubtopicSearch()}>{value}  </Button>
+
+      {loading ? <Spinner animation="border" size="sm" /> : null}
     </span>
   );
 };
