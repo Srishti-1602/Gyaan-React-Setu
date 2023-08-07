ba8160ce9f49e5693a6d515f51211682ac1a6b24
Srishti Agrawal <88929699+Srishti-1602@users.noreply.github.com>
2023-08-07 16:19:01 +0530

    notes-dash

--- src/Dashboard/DashboardComponents/My-Notes.jsx
+++ src/Dashboard/DashboardComponents/My-Notes.jsx
@@ -1,11 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import NoteCard from './NoteCard';
+import React, { useEffect, useState } from 'react'
+import NoteCard from './NoteCard'
 
 const MyNotes = () => {
   return (
     <div className='row row-notes-display'>
-      <NoteCard noteTitle="Title" noteSubject="Subject" noteCreator="Creator" noteLastEdited="Date" remixNum="1" starsNum="12" viewsNum="123" />
-      <NoteCard noteTitle="Title" noteSubject="Subject" noteCreator="Creator" noteLastEdited="Date" remixNum="1" starsNum="12" viewsNum="123" />
+      <NoteCard
+        noteTitle='Title'
+        noteSubject='Subject'
+        noteCreator='Creator'
+        noteLastEdited='Date'
+        remixNum='1'
+        starsNum='12'
+        viewsNum='123'
+      />
+      <NoteCard
+        noteTitle='Title'
+        noteSubject='Subject'
+        noteCreator='Creator'
+        noteLastEdited='Date'
+        remixNum='1'
+        starsNum='12'
+        viewsNum='123'
+      />
+      <NoteCard
+        noteTitle='Title'
+        noteSubject='Subject'
+        noteCreator='Creator'
+        noteLastEdited='Date'
+        remixNum='1'
+        starsNum='12'
+        viewsNum='123'
+      />
+      <NoteCard
+        noteTitle='Title'
+        noteSubject='Subject'
+        noteCreator='Creator'
+        noteLastEdited='Date'
+        remixNum='1'
+        starsNum='12'
+        viewsNum='123'
+      />
     </div>
   )
 }
