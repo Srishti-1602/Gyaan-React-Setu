65fee7efcd6d9bf26426665cbc0d08be6fb1324b
Srishti Agrawal <88929699+Srishti-1602@users.noreply.github.com>
2023-08-14 00:03:24 +0530

    dropdown fcss

--- src/Main/MainComponents/Dropdown.jsx
+++ src/Main/MainComponents/Dropdown.jsx
@@ -1,55 +1,52 @@
-import '../main.css';
-import React, {useState} from 'react';
-import FeedbackComponent from './FeedbackSection';
-import CommentSection from './CommentSection';
-import Remix from './Remix';
+import '../main.css'
+import React, { useState } from 'react'
+import FeedbackComponent from './FeedbackSection'
+import CommentSection from './CommentSection'
+import Remix from './Remix'
 
 const Dropdown = () => {
-    const [selectedOption, setSelectedOption] = useState(null);
-const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-const handleOptionClick = option => {
+  const [selectedOption, setSelectedOption] = useState(null)
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+  const handleOptionClick = option => {
     setSelectedOption(option)
     setIsDropdownOpen(false)
-};
+  }
   return (
-    <div className='custom-dropdown'>
-    <div
-      className='dropdown-header'
-      onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-    >
-      Select an option
-      <span className='dropdown-arrow'>&#9662;</span>
-    </div>
-    {isDropdownOpen && (
-      <div className='dropdown-options'>
+    <div className='dropdown-container'>
+      <div className='custom-dropdown'>
         <div
-          className='option'
-          onClick={() => handleOptionClick('remix')}
+          className='dropdown-header'
+          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
         >
-          <Remix />
+          Select an option
+          <span className='dropdown-arrow'>&#9662;</span>
         </div>
-        <div
-          className='option'
-          onClick={() => handleOptionClick('comment')}
-        >
-          <div onClick={e => e.stopPropagation()}>
-            <CommentSection />
+        {isDropdownOpen && (
+          <div className='dropdown-options'>
+            <div className='option' onClick={() => handleOptionClick('remix')}>
+              <Remix />
+            </div>
+            <div
+              className='option'
+              onClick={() => handleOptionClick('comment')}
+            >
+              <div onClick={e => e.stopPropagation()}>
+                <CommentSection />
+              </div>
+            </div>
+            <div
+              className='option'
+              onClick={() => handleOptionClick('feedback')}
+            >
+              <div onClick={e => e.stopPropagation()}>
+                <FeedbackComponent />
+              </div>
+            </div>
           </div>
-    
-        </div>
-        <div
-          className='option'
-          onClick={() => handleOptionClick('feedback')}
-        >
-          <div onClick={e => e.stopPropagation()}>
-            <FeedbackComponent />
-          </div>
-
-        </div>
+        )}
       </div>
-    )}
-  </div>
+    </div>
   )
 }
 
-export default Dropdown;
+export default Dropdown
