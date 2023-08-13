86c11f30d3f2c30a21e9a5b5149322ca152f3b3f
Srishti Agrawal <88929699+Srishti-1602@users.noreply.github.com>
2023-08-13 14:00:34 +0530

    dropdown

--- src/Main/MainComponents/FeedbackSection.jsx
+++ src/Main/MainComponents/FeedbackSection.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from 'react'
 import FeedbackIcon from '../../icons/feedback-fill.png'
-import '../main.css';
+import '../main.css'
 
 const FeedbackSection = ({ isVisible, onClose }) => {
-  const [feedbackText, setFeedbackText] = useState('');
+  const [feedbackText, setFeedbackText] = useState('')
 
-  const handleFeedbackChange = (event) => {
-    setFeedbackText(event.target.value);
-  };
+  const handleFeedbackChange = event => {
+    setFeedbackText(event.target.value)
+  }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = event => {
+    event.preventDefault()
     // Handle submission of feedback
-    console.log('Feedback submitted:', feedbackText);
-    setFeedbackText('');
-  };
+    console.log('Feedback submitted:', feedbackText)
+    setFeedbackText('')
+  }
 
   return (
     <div className={`feedback-section ${isVisible ? 'visible' : ''}`}>
@@ -36,31 +36,34 @@ const FeedbackSection = ({ isVisible, onClose }) => {
         <button type='submit'>Submit</button>
       </form>
     </div>
-  );
-};
+  )
+}
 
 const FeedbackButton = ({ onClick }) => {
   return (
-    <button className='feedback-button' onClick={onClick}>
+    <button className='feedback-button-but' onClick={onClick}>
       <img src={FeedbackIcon} alt='Feedback' className='feedback-button' />
+      Feedback
     </button>
-  );
-};
+  )
+}
 
 const FeedbackComponent = () => {
-  const rectNotesRef = useRef(null);
-  const [showFeedback, setShowFeedback] = useState(false);
+  const rectNotesRef = useRef(null)
+  const [showFeedback, setShowFeedback] = useState(false)
 
   const toggleFeedback = () => {
-    setShowFeedback((prevShowFeedback) => !prevShowFeedback);
-  };
+    setShowFeedback(prevShowFeedback => !prevShowFeedback)
+  }
 
   return (
     <div>
       <FeedbackButton onClick={toggleFeedback} />
-      {showFeedback && <FeedbackSection isVisible={true} onClose={toggleFeedback} />}
+      {showFeedback && (
+        <FeedbackSection isVisible={true} onClose={toggleFeedback} />
+      )}
     </div>
-  );
-};
+  )
+}
 
-export default FeedbackComponent;
+export default FeedbackComponent
