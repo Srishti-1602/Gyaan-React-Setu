95ab5ae3b9e40bee01e27fe8be6631ca41d50144
Srishti Agrawal <88929699+Srishti-1602@users.noreply.github.com>
2023-08-16 21:45:38 +0530

    Css for Main page

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,157 +1,156 @@
-import React, { useState, useEffect } from "react";
-import "./main.css";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getDatabase, ref, onValue } from "firebase/database";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
-import Navbar1 from "../NewNav/NewNav";
+import React, { useState, useEffect } from 'react'
+import './main.css'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { getDatabase, ref, onValue } from 'firebase/database'
+import { getFirestore, doc, getDoc } from 'firebase/firestore'
+import Navbar1 from '../NewNav/NewNav'
 /* import JsonNode from './JSONnode/JsonNodeOld'; */
-import JsonNode from "./JSONnode/JsonNode";
-import Search from "./Search/Search";
-import SaveButton from "./MainComponents/SaveButton";
-import LoadingBar from "./Search/loadingAnimation/loadingAnimation";
-import { getQueryId } from "./Search/queryIdManager";
-import { useLocation } from "react-router-dom";
+import JsonNode from './JSONnode/JsonNode'
+import Search from './Search/Search'
+import SaveButton from './MainComponents/SaveButton'
+import LoadingBar from './Search/loadingAnimation/loadingAnimation'
+import { getQueryId } from './Search/queryIdManager'
+import { useLocation } from 'react-router-dom'
 //import data from './data'
-import Dropdown from "./MainComponents/Dropdown";
-import PublicNoteData from "./MainComponents/publicNoteView";
+import Dropdown from './MainComponents/Dropdown'
+import PublicNoteData from './MainComponents/publicNoteView'
 
-const data = {};
+const data = {}
 
-export default function Main(props) {
-	/* Getting User ID */
-	const [userId, setUserId] = useState(null); // Store the userId here
-	const [isLoggedIn, setIsLoggedIn] = useState(false); // Store the isLoggedIn state here
+export default function Main (props) {
+  /* Getting User ID */
+  const [userId, setUserId] = useState(null) // Store the userId here
+  const [isLoggedIn, setIsLoggedIn] = useState(false) // Store the isLoggedIn state here
 
-	useEffect(() => {
-		const auth = getAuth();
-		const unsubscribe = onAuthStateChanged(auth, (user) => {
-			if (user) {
-				setIsLoggedIn(true);
-				setUserId(user.uid); // Set the userId if the user is logged in
-			} else {
-				setIsLoggedIn(false);
-				setUserId(null); // Reset the userId if the user is not logged in
-			}
-		});
+  useEffect(() => {
+    const auth = getAuth()
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (user) {
+        setIsLoggedIn(true)
+        setUserId(user.uid) // Set the userId if the user is logged in
+      } else {
+        setIsLoggedIn(false)
+        setUserId(null) // Reset the userId if the user is not logged in
+      }
+    })
 
-		return () => {
-			unsubscribe();
-		};
-	}, []);
-	/* End of Getting User ID */
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+  /* End of Getting User ID */
 
-	/* Handling Search and Giving JSON output for rendering */
-	const [jsonData, setJsonData] = useState(data);
+  /* Handling Search and Giving JSON output for rendering */
+  const [jsonData, setJsonData] = useState(data)
 
-	const handleSetData = (newData) => {
-		setJsonData(newData);
-		console.log(newData);
-	};
+  const handleSetData = newData => {
+    setJsonData(newData)
+    console.log(newData)
+  }
 
-	const handleSearch = (searchQuery) => {
-		console.log("Search query:", searchQuery);
-		setJsonData(searchQuery);
-	};
-	console.log(jsonData);
-	/* End of Handling Search and Giving JSON output for rendering */
+  const handleSearch = searchQuery => {
+    console.log('Search query:', searchQuery)
+    setJsonData(searchQuery)
+  }
+  console.log(jsonData)
+  /* End of Handling Search and Giving JSON output for rendering */
 
-	/* Query Processing status */
-	const [queryStatus, setQueryStatus] = useState("static"); // Add query status state
-	const queryId = getQueryId(); // Get the queryId from the queryIdManager
-	const queryRef = `users/${userId}/Queries/${queryId}`;
+  /* Query Processing status */
+  const [queryStatus, setQueryStatus] = useState('static') // Add query status state
+  const queryId = getQueryId() // Get the queryId from the queryIdManager
+  const queryRef = `users/${userId}/Queries/${queryId}`
 
-	useEffect(() => {
-		const database = getDatabase();
-		const queryStatusRef = ref(
-			database,
-			`users/${userId}/Queries/currentQueryStatus`
-		);
-		onValue(queryStatusRef, (snapshot) => {
-			const status = snapshot.val() || "static";
-			console.log(status);
-			setQueryStatus(status);
-		});
-	}, [userId]);
-	/* End of Query Processing status */
+  useEffect(() => {
+    const database = getDatabase()
+    const queryStatusRef = ref(
+      database,
+      `users/${userId}/Queries/currentQueryStatus`
+    )
+    onValue(queryStatusRef, snapshot => {
+      const status = snapshot.val() || 'static'
+      console.log(status)
+      setQueryStatus(status)
+    })
+  }, [userId])
+  /* End of Query Processing status */
 
-	/* Fetch Saved Note Id */
-	const location = useLocation();
-	const [savedNoteId, setSavedNoteId] = useState(null);
-	const [publicNoteId, setPublicNoteId] = useState(null);
+  /* Fetch Saved Note Id */
+  const location = useLocation()
+  const [savedNoteId, setSavedNoteId] = useState(null)
+  const [publicNoteId, setPublicNoteId] = useState(null)
 
-	useEffect(() => {
-		const searchParams = new URLSearchParams(location.search);
-		const noteIdParam = searchParams.get("NId");
-		setPublicNoteId(noteIdParam);
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search)
+    const noteIdParam = searchParams.get('NId')
+    setPublicNoteId(noteIdParam)
 
-		if (noteIdParam) {
-			const database = getDatabase();
-			const savedNoteIdRef = ref(database, `notes/${noteIdParam}/noteContent`); // Replace QUERY_ID and CURRENT_QUERY_ID with actual values
+    if (noteIdParam) {
+      const database = getDatabase()
+      const savedNoteIdRef = ref(database, `notes/${noteIdParam}/noteContent`) // Replace QUERY_ID and CURRENT_QUERY_ID with actual values
 
-			onValue(savedNoteIdRef, (snapshot) => {
-				const savedNote = snapshot.val() || null;
-				console.log(savedNote);
-				setSavedNoteId(savedNote);
-				fetchData(savedNote); // Call fetchData with the updated savedNoteId
-			});
-		}
-	}, [userId]);
+      onValue(savedNoteIdRef, snapshot => {
+        const savedNote = snapshot.val() || null
+        console.log(savedNote)
+        setSavedNoteId(savedNote)
+        fetchData(savedNote) // Call fetchData with the updated savedNoteId
+      })
+    }
+  }, [userId])
 
-	/* Fetch data from Firestore based on queryId */
-	const fetchData = async (savedNoteId) => {
-		const searchParams = new URLSearchParams(location.search);
-		const noteIdParam = searchParams.get("NId");
+  /* Fetch data from Firestore based on queryId */
+  const fetchData = async savedNoteId => {
+    const searchParams = new URLSearchParams(location.search)
+    const noteIdParam = searchParams.get('NId')
 
-		if (noteIdParam && savedNoteId) {
-			const firestore = getFirestore();
-			console.log(savedNoteId);
-			const docRef = doc(firestore, "notes", savedNoteId);
-			const docSnap = await getDoc(docRef);
+    if (noteIdParam && savedNoteId) {
+      const firestore = getFirestore()
+      console.log(savedNoteId)
+      const docRef = doc(firestore, 'notes', savedNoteId)
+      const docSnap = await getDoc(docRef)
 
-			if (docSnap.exists()) {
-				const noteData = docSnap.data().jsonData; // Retrieve data from the 'jsonData' node
-				const parsedData = JSON.parse(noteData); // Parse the data using JSON.parse()
-				setJsonData(parsedData);
-			} else {
-				console.log("Note does not exist");
-			}
-		}
-	};
+      if (docSnap.exists()) {
+        const noteData = docSnap.data().jsonData // Retrieve data from the 'jsonData' node
+        const parsedData = JSON.parse(noteData) // Parse the data using JSON.parse()
+        setJsonData(parsedData)
+      } else {
+        console.log('Note does not exist')
+      }
+    }
+  }
 
-	useEffect(() => {
-		fetchData(savedNoteId); // Call fetchData initially with the savedNoteId
-	}, [location.search, savedNoteId]);
-	/* End of Fetch data from Firestore based on queryId */
+  useEffect(() => {
+    fetchData(savedNoteId) // Call fetchData initially with the savedNoteId
+  }, [location.search, savedNoteId])
+  /* End of Fetch data from Firestore based on queryId */
 
-	return (
-		<div className="MainContent">
-			<Navbar1 />
+  return (
+    <div className='MainContent'>
+      <Navbar1 />
 
-			<div className="your-topics">
-				<Search onSearch={handleSearch} />
-			</div>
+      <div className='your-topics'>
+        <Search onSearch={handleSearch} />
+      </div>
 
-			<div className="rectnotes">
-				<div className="note-info"></div>
-				<div className="User-Container">
-					<div className="icon">
-						<SaveButton
-							jsonData={jsonData}
-							queryRef={queryRef}
-							isLoggedIn={isLoggedIn}
-							UserID={userId}
-							queryId={queryId}
-						/>
-						<Dropdown userId={userId} noteId={publicNoteId} />
-					</div>
-
-					<PublicNoteData userId={userId} noteId={publicNoteId} />
-				</div>
-				<div id="tree-view">
-					{queryStatus === "processing" ? <LoadingBar /> : null}
-					<JsonNode data={jsonData} setData={handleSetData} />
-				</div>
-			</div>
-		</div>
-	);
+      <div className='rectnotes'>
+        <div className='note-info'></div>
+        <div className='User-Container'>
+          <PublicNoteData userId={userId} noteId={publicNoteId} />
+          <div className='icon'>
+            <SaveButton
+              jsonData={jsonData}
+              queryRef={queryRef}
+              isLoggedIn={isLoggedIn}
+              UserID={userId}
+              queryId={queryId}
+            />
+            <Dropdown userId={userId} noteId={publicNoteId} />
+          </div>
+        </div>
+        <div id='tree-view'>
+          {queryStatus === 'processing' ? <LoadingBar /> : null}
+          <JsonNode data={jsonData} setData={handleSetData} />
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/Main/MainComponents/publicNoteView.jsx
+++ src/Main/MainComponents/publicNoteView.jsx
@@ -1,89 +1,238 @@
-import "../main.css";
-import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import { getDatabase, ref, onValue, get } from "firebase/database";
-import Remix from "../../IconUtils/Remix";
-import Views from "../../IconUtils/Views";
-import Star from "../../IconUtils/Star";
+// import "../main.css";
+// import React, { useState, useEffect } from "react";
+// import { useLocation } from "react-router-dom";
+// import { getDatabase, ref, onValue, get } from "firebase/database";
+// import Remix from "../../IconUtils/Remix";
+// import Views from "../../IconUtils/Views";
+// import Star from "../../IconUtils/Star";
+
+// const PublicNoteData = ({ userId, noteId }) => {
+// 	const location = useLocation();
+// 	const queryParams = new URLSearchParams(location.search);
+// 	const hasNIdParam = queryParams.has("NId");
+// 	const [createdBy, setCreatedBy] = useState(null);
+// 	const [lastEditedDate, setLastEditedDate] = useState(null);
+// 	const [remixNum, setRemixNum] = useState(0);
+// 	const [viewsNum, setViewsNum] = useState(0);
+// 	const [noteTitle, setNoteTitle] = useState(null);
+// 	// const [isDropdown, setIsDropdown] = useState(false)
+
+// 	useEffect(() => {
+// 		if (!hasNIdParam) {
+// 			return; // Do nothing if NId parameter is not present
+// 		}
+
+// 		const database = getDatabase();
+// 		const noteRef = ref(database, `notes/${noteId}`);
+
+// 		onValue(noteRef, (snapshot) => {
+// 			const noteData = snapshot.val();
+// 			if (noteData) {
+// 				setCreatedBy(noteData.created_by);
+// 				setLastEditedDate(new Date(noteData.created_at).toLocaleDateString());
+// 				setRemixNum(noteData.remix);
+// 				setViewsNum(noteData.views);
+// 				setNoteTitle(noteData.title);
+// 			}
+// 		});
+// 		// setIsDropdown(window.innerWidth <= 900)
+// 	}, [hasNIdParam, noteId]);
+
+// 	const [creatorUsername, setCreatorUsername] = useState(""); // State to hold the creator's username
+
+// 	useEffect(() => {
+// 		const database = getDatabase();
+// 		const usersRef = ref(database, `users/${createdBy}`);
+
+// 		// Fetch the username from the users node based on noteCreator
+// 		get(usersRef)
+// 			.then((snapshot) => {
+// 				if (snapshot.exists()) {
+// 					const userData = snapshot.val();
+// 					console.log("userData:", userData);
+// 					setCreatorUsername(userData.username || "Unknown User");
+// 				} else {
+// 					setCreatorUsername("Unknown User");
+// 				}
+// 			})
+// 			.catch((error) => {
+// 				console.error("Error fetching username:", error);
+// 				setCreatorUsername("Unknown User");
+// 			});
+// 	}, [createdBy]);
+
+// 	if (!hasNIdParam) {
+// 		return null; // Return null if NId parameter is not present
+// 	}
+
+// 	return (
+// 		<div className="User-Main">
+// 			<span className="TitleUser">{`Title: ${noteTitle}`}</span>
+// 			<span className="MainUser">
+// 				{createdBy === userId ? "Your Note" : `Created By: ${creatorUsername}`}
+// 			</span>
+// 			<span className="LastDateUser" style={{ color: "white" }}>
+// 				{`Last edit: ${lastEditedDate}`}
+// 			</span>
+// 			<span className="IconUser" style={{ color: "white" }}>
+// 				<Star userId={userId} noteId={noteId} />
+// 				{" | "}
+// 				<Remix remixNum={remixNum} /> {" | "}
+// 				<Views viewsNum={viewsNum} />
+// 			</span>
+// 			{/* </>
+//       )} */}
+// 		</div>
+// 	);
+// };
+
+// export default PublicNoteData;
+
+import '../main.css'
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import { getDatabase, ref, onValue, get } from 'firebase/database'
+import Remix from '../../IconUtils/Remix'
+import Views from '../../IconUtils/Views'
+import Star from '../../IconUtils/Star'
 
 const PublicNoteData = ({ userId, noteId }) => {
-	const location = useLocation();
-	const queryParams = new URLSearchParams(location.search);
-	const hasNIdParam = queryParams.has("NId");
-	const [createdBy, setCreatedBy] = useState(null);
-	const [lastEditedDate, setLastEditedDate] = useState(null);
-	const [remixNum, setRemixNum] = useState(0);
-	const [viewsNum, setViewsNum] = useState(0);
-	const [noteTitle, setNoteTitle] = useState(null);
-	// const [isDropdown, setIsDropdown] = useState(false)
+  const location = useLocation()
+  const queryParams = new URLSearchParams(location.search)
+  const hasNIdParam = queryParams.has('NId')
+  const [createdBy, setCreatedBy] = useState(null)
+  const [lastEditedDate, setLastEditedDate] = useState(null)
+  const [remixNum, setRemixNum] = useState(0)
+  const [viewsNum, setViewsNum] = useState(0)
+  const [noteTitle, setNoteTitle] = useState(null)
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 900)
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+  const [selectedOption, setSelectedOption] = useState(null)
 
-	useEffect(() => {
-		if (!hasNIdParam) {
-			return; // Do nothing if NId parameter is not present
-		}
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 900)
+    }
 
-		const database = getDatabase();
-		const noteRef = ref(database, `notes/${noteId}`);
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize)
 
-		onValue(noteRef, (snapshot) => {
-			const noteData = snapshot.val();
-			if (noteData) {
-				setCreatedBy(noteData.created_by);
-				setLastEditedDate(new Date(noteData.created_at).toLocaleDateString());
-				setRemixNum(noteData.remix);
-				setViewsNum(noteData.views);
-				setNoteTitle(noteData.title);
-			}
-		});
-		// setIsDropdown(window.innerWidth <= 900)
-	}, [hasNIdParam, noteId]);
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
-	const [creatorUsername, setCreatorUsername] = useState(""); // State to hold the creator's username
+  useEffect(() => {
+    if (!hasNIdParam) {
+      return // Do nothing if NId parameter is not present
+    }
 
-	useEffect(() => {
-		const database = getDatabase();
-		const usersRef = ref(database, `users/${createdBy}`);
+    const database = getDatabase()
+    const noteRef = ref(database, `notes/${noteId}`)
 
-		// Fetch the username from the users node based on noteCreator
-		get(usersRef)
-			.then((snapshot) => {
-				if (snapshot.exists()) {
-					const userData = snapshot.val();
-					console.log("userData:", userData);
-					setCreatorUsername(userData.username || "Unknown User");
-				} else {
-					setCreatorUsername("Unknown User");
-				}
-			})
-			.catch((error) => {
-				console.error("Error fetching username:", error);
-				setCreatorUsername("Unknown User");
-			});
-	}, [createdBy]);
+    onValue(noteRef, snapshot => {
+      const noteData = snapshot.val()
+      if (noteData) {
+        setCreatedBy(noteData.created_by)
+        setLastEditedDate(new Date(noteData.created_at).toLocaleDateString())
+        setRemixNum(noteData.remix)
+        setViewsNum(noteData.views)
+        setNoteTitle(noteData.title)
+      }
+    })
+    // setIsDropdown(window.innerWidth <= 900)
+  }, [hasNIdParam, noteId])
 
-	if (!hasNIdParam) {
-		return null; // Return null if NId parameter is not present
-	}
+  const [creatorUsername, setCreatorUsername] = useState('') // State to hold the creator's username
 
-	return (
-		<div className="User-Main">
-			<span className="TitleUser">{`Title: ${noteTitle}`}</span>
-			<span className="MainUser">
-				{createdBy === userId ? "Your Note" : `Created By: ${creatorUsername}`}
-			</span>
-			<span className="LastDateUser" style={{ color: "white" }}>
-				{`Last edit: ${lastEditedDate}`}
-			</span>
-			<span className="IconUser" style={{ color: "white" }}>
-				<Star userId={userId} noteId={noteId} />
-				{" | "}
-				<Remix remixNum={remixNum} /> {" | "}
-				<Views viewsNum={viewsNum} />
-			</span>
-			{/* </>
-      )} */}
-		</div>
-	);
-};
+  useEffect(() => {
+    const database = getDatabase()
+    const usersRef = ref(database, `users/${createdBy}`)
 
-export default PublicNoteData;
+    // Fetch the username from the users node based on noteCreator
+    get(usersRef)
+      .then(snapshot => {
+        if (snapshot.exists()) {
+          const userData = snapshot.val()
+          console.log('userData:', userData)
+          setCreatorUsername(userData.username || 'Unknown User')
+        } else {
+          setCreatorUsername('Unknown User')
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching username:', error)
+        setCreatorUsername('Unknown User')
+      })
+  }, [createdBy])
+
+  if (!hasNIdParam) {
+    return null // Return null if NId parameter is not present
+  }
+
+  //   const handleDropdownClick = () => {
+  //     setIsDropdownOpen(!isDropdownOpen)
+  //   }
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen)
+  }
+
+  return (
+    <div className='User-Main'>
+      {isSmallScreen && (
+        <div className={`custom-dropdownUser ${isDropdownOpen ? 'open' : ''}`}>
+          <div className='dropdown-header' onClick={toggleDropdown}>
+            <span className='TitleUser'>{`Title: ${noteTitle}`}</span>
+            <span className='dropdown-arrow'>&#9662;</span>
+          </div>
+          {isDropdownOpen && (
+            <div className='dropdown-options-User'>
+              <div className='option-User'>
+                <span className='MainUser'>
+                  {createdBy === userId
+                    ? 'Your Note'
+                    : `Created By: ${creatorUsername}`}
+                </span>
+              </div>
+              <div className='option-User'>
+                <span className='LastDateUser'>
+                  {`Last edit: ${lastEditedDate}`}
+                </span>
+              </div>
+              <div className='option-User'>
+                <span className='IconUser'>
+                  <Star userId={userId} noteId={noteId} />
+                  {' | '}
+                  <Remix remixNum={remixNum} /> {' | '}
+                  <Views viewsNum={viewsNum} />
+                </span>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+      {!isSmallScreen && (
+        <div className='User-Main'>
+          <span className='TitleUser'>{`Title: ${noteTitle}`}</span>
+          <span className='MainUser'>
+            {createdBy === userId
+              ? 'Your Note'
+              : `Created By: ${creatorUsername}`}
+          </span>
+          <span className='LastDateUser' style={{ color: 'white' }}>
+            {`Last edit: ${lastEditedDate}`}
+          </span>
+          <span className='IconUser' style={{ color: 'white' }}>
+            <Star userId={userId} noteId={noteId} />
+            {' | '}
+            <Remix remixNum={remixNum} /> {' | '}
+            <Views viewsNum={viewsNum} />
+          </span>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default PublicNoteData
