b83b3add2828e0dd84aae830f060e12b26010251
Sukritee Sharma <87569188+aceta-minophen@users.noreply.github.com>
2023-08-19 01:18:34 +0530

    comment write to fb done

--- src/Dashboard/DashboardComponents/Remixed-Notes.jsx
+++ src/Dashboard/DashboardComponents/Remixed-Notes.jsx
@@ -1,28 +1,31 @@
 import React, { useEffect, useState } from "react";
 import NoteCard from "./NoteCard";
-// import Views from '../../icons/spy-fill.png'
+import { getDatabase, ref, onValue } from "firebase/database";
 
-const RemixedNotes = () => {
+const RemixedNotes = ({ userId }) => {
+	const [notes, setNotes] = useState([]); // State to hold the fetched notes
+
+	useEffect(() => {
+		const database = getDatabase();
+		const userNotesRef = ref(database, `users/${userId}/RemixedNotes`);
+
+		onValue(userNotesRef, (snapshot) => {
+			const notesData = snapshot.val();
+			if (notesData) {
+				const notesArray = Object.keys(notesData).map((noteId) => ({
+					noteId,
+					...notesData[noteId],
+				}));
+				setNotes(notesArray);
+			}
+		});
+	}, [userId]);
+
 	return (
 		<div className="row row-notes-display">
-			<NoteCard
-				noteTitle="Title"
-				noteSubject="Subject"
-				noteCreator="Creator"
-				noteLastEdited="Date"
-				remixNum="1"
-				starsNum="12"
-				viewsNum="123"
-			/>
-			<NoteCard
-				noteTitle="Title"
-				noteSubject="Subject"
-				noteCreator="Creator"
-				noteLastEdited="Date"
-				remixNum="1"
-				starsNum="12"
-				viewsNum="123"
-			/>
+			{notes.map((note) => (
+				<NoteCard key={note.noteId} noteId={note.noteId} userId={userId} />
+			))}
 		</div>
 	);
 };
